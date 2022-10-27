9246f93e40468b85d0e07aa94bb2033e1322672d
Issam Elmakhfi <ielmakhf@e1r4p10.1337.ma>
2022-10-28 00:58:46 +0100

    pop

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,21 @@
+char *get_next_line(int fd)
+{
+    int     i = 0;
+    int     rd = 0;
+    char    character;
+    char     *buffer = malloc(10000);
+
+    while ((rd = read(fd, &character, 1)) > 0)
+    {
+        buffer[i++] = character;
+        if (character == '\n')
+            break;
+    }
+    if ((!buffer[i - 1] && !rd) || rd == -1)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    buffer[i] =  '\0';
+    return(buffer);
+}
